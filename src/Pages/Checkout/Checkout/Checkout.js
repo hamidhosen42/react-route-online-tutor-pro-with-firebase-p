b1bc5835aa84a01f3ccbe7d90046adcc122bd017
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-04-17 22:53:14 +0600

    add login

--- src/Pages/Checkout/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout/Checkout.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 
 const Checkout = () => {
     return (
-        <div>
-            <h2>Please Checkout your booking</h2>
-        </div>
+      <div>
+        <h2 className="text-center mt-5 mb-4">Welcome to the Course</h2>
+        <Form className="container w-50 mx-auto margin-button">
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="name@example.com" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Description</Form.Label>
+            <Form.Control as="textarea" rows={3} />
+          </Form.Group>
+        </Form>
+      </div>
     );
 };
 
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams } from "react-router-dom";
+import './ServiceDetails.css';
 
 const ServiceDetail = () => {
-    const { serviceId } = useParams();
-    return (
-        <div>
-            <h2>Welcome to detail: {serviceId}</h2>
-            <div className='text-center'>
-                <Link to="/checkout">
-                    <button className='btn btn-primary'>Proceed Checkout</button>
-                </Link>
-            </div>
-        </div>
-    );
+  const { serviceId } = useParams();
+
+  var serviceName;
+
+  for (var i = 0; i < 6; i++) {
+    if (serviceId == 1) {
+      serviceName = "MATH";
+    } else if (serviceId == 2) {
+      serviceName = "PHYSICS";
+    } else if (serviceId == 3) {
+      serviceName = "CHEMISTRY";
+    } else if (serviceId == 4) {
+      serviceName = "BIOLOGY";
+    } else if (serviceId == 5) {
+      serviceName = "ENGLISH";
+    } else {
+      serviceName = "BANGLA";
+    }
+  }
+  return (
+    <div>
+      <h2 className="text-center mt-5">
+        Please Checkout your {serviceName} Course
+      </h2>
+      <div className="text-center mt-5 margin-button">
+        <Link to="/checkout">
+          <button className="btn btn-primary">Proceed Checkout</button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default ServiceDetail;
