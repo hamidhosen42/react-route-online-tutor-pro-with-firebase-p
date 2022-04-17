86ae729936a26ea7b836395ee2c094a13813ed18
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-04-18 00:32:51 +0600

    about section add

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import hamid from "../../images/hamid.jpg";
 
 const About = () => {
-    return (
-        <div>
-            <h2>This is About us</h2>
-        </div>
-    );
+  return (
+    <div className="text-center container mt-5">
+      <Card>
+        <Card.Img
+          variant="top"
+          style={{ height: "50%" }}
+          className="mx-auto w-50"
+          src={hamid}
+        />
+        <Card.Body>
+          <Card.Title>Name: Hamid Hosen</Card.Title>
+          <Card.Text>
+            For the next four months I am trying to be a full stack web
+            development. And trying to learn Flutter Mobile App by myself.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default About;
