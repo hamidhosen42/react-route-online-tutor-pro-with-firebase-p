f7d32788863784a4999db40250643053a75c66fe
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-04-18 00:14:45 +0600

    404-not found

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import sleeping from '../../../images/sleeping.jpg';
+import sleeping from '../../../images/404.png';
 
 const NotFound = () => {
     return (
         <div>
-            <h2 className='text-primary text-center'>Mechanic is sleeping</h2>
-            <img className='w-100' src={sleeping} alt="" />
+            <img className='w-100 ' src={sleeping} alt="" />
         </div>
     );
 };
